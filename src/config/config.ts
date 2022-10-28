d578fd67a36a90823680fa0f8e68ce4cea10220a
STRRAAA <stracovskymichal@gmail.com>
2022-10-28 22:40:51 +0200

    fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,8 +7,8 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.bcl8bsa.mongodb.net/`;
 
-const SERVER_PORT = process.env.SERVER_PORT
-  ? Number(process.env.SERVER_PORT)
+const PORT = process.env.PORT
+  ? Number(process.env.PORT)
   : 1337;
 
 export const config = {
@@ -16,6 +16,6 @@ export const config = {
     url: MONGO_URL,
   },
   server: {
-    port: SERVER_PORT,
+    port: PORT,
   },
 };
